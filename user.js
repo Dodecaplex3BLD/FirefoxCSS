3fe8352956208262ef53575c340096571fc330ae
Josiah Gilmour <104062658+Dodecaplex3BLD@users.noreply.github.com>
2025-04-04 18:03:11 +1100

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,4 @@
 user_pref("sidebar.verticalTabs", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets",true);
+user_pref("browser.tabs.groups.enabled",true);
