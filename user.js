01e227eef7819c25b6d1289366ae58b60eac59f2
Josiah Gilmour <104062658+Dodecaplex3BLD@users.noreply.github.com>
2025-04-30 13:15:38 +1000

    Update user.js

--- user.js
+++ user.js
@@ -2,3 +2,6 @@ user_pref("sidebar.verticalTabs", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets",true);
 user_pref("browser.tabs.groups.enabled",true);
+user_pref("sidebar.expandOnHover",true);
+user_pref("sidebar.animation.expand-on-hover.duration-ms",100);
+user_pref("browser.profiles.enabled",true);
